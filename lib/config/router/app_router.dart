7ac14ef3f3bec7d6160171edd757ad99abebf691
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-22 01:55:45 -0400

    feat: add environment variables configuration code

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,14 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:cinemapedia_flutter/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      pageBuilder: (context, state) => const HomeScreen() as dynamic,
-    ),
-  ],
-);
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  ),
+]);
